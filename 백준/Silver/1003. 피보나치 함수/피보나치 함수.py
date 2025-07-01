01beeac1e0adc3af2aa6affd9cedf07625e88f75
rlaqudwn1 <162764129+rlaqudwn1@users.noreply.github.com>
2025-07-01 18:25:38 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,15 @@
+t= int(input())
+for _ in range(t):
+    n = int(input())
+    if(n==0):
+        print("1 0")
+    else:
+        dp = [[0,0] for _ in range (n+2)]
+        dp[0] = [1,0]
+        dp[1] = [0,1]
+        for i in range(2,n+1):
+            dp[i][0] = dp[i-1][0] + dp[i-2][0]
+            dp[i][1] = dp[i-1][1] + dp[i-2][1]
+        print(f"{dp[n][0]} {dp[n][1]}")
+
+    
