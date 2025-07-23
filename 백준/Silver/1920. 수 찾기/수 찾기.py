9c85880d3b7bf452271509e5db78dfd267c911a3
rlaqudwn1 <162764129+rlaqudwn1@users.noreply.github.com>
2025-07-23 17:08:50 +0900

    [Silver IV] Title: 수 찾기, Time: 960 ms, Memory: 48624 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,27 @@
+import sys
+
+N = int(input())
+A= list(map(int, input().split()))
+M = int(sys.stdin.readline())
+B= list(map(int, input().split()))
+
+
+A.sort()
+
+for i in range(M):
+    left =0
+    right = len(A)-1
+    correct = False
+    while(left<= right):
+        mid = (left+right)//2
+        if(A[mid]<B[i]):
+            left=mid+1
+        if(A[mid]>B[i]):
+            right=mid-1
+        if(A[mid]==B[i]):
+            correct = True
+            break
+    if(correct==True):
+        print("1")
+    else:
+        print("0")
